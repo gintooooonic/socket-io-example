56440583aa5ffabfcbb1a7e2a191fe82ab26a389
gintooooonic <gintooooonic@gmail.com>
2021-10-10 23:35:31 +0900

    패키지 제거

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
-const cookie = require("cookie");
-const iconv = require("iconv-lite");
 const port = process.env.PORT || 3000;
 
 const players = {};
